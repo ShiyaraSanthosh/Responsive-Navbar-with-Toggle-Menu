bf3d030430ec4ac2d6e2761d095e345bf4fd4b68
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-06-28 14:54:01 +0530

    responsive

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ Icon.onclick = function() {
     menu.classList.toggle("active");
     if(menu.classList.contains("active")) {
         Icon.src = "images/close.png.png";
+        Icon.classList.add("close_button");
     } else {
         Icon.src = "images/menu.png";
+        Icon.classList.remove("close_button");
     }
    
 }
