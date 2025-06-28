ec41f25e4deb5208057c76abdc2e53fb460b7113
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-06-28 12:18:16 +0530

    responsive

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ const Icon = document.getElementById("icon");
 const menu = document.querySelector(".menu");
 Icon.onclick = function() {
     menu.classList.toggle("active");
+    if(menu.classList.contains("active")) {
+        Icon.src = "images/close.png.png";
+    } else {
+        Icon.src = "images/menu.png";
+    }
    
 }
